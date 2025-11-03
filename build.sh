aa71049b2320fd27954d29577cbdf64f31c5604f
Danionetti <danionetti20@gmail.com>
2025-11-03 16:56:54 +0100

    Fix: Usar 'tailwind install' en lugar de 'npm install'

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -o errexit
 # 1. Instalar dependencias de Python
 pip install -r requirements.txt
 
-# 2. Instalar dependencias de Node.js (¡LA LÍNEA QUE FALTABA!)
-npm install
+# 2. Instalar dependencias de Node (¡LA FORMA CORRECTA!)
+#    Esto ejecutará 'npm install' dentro de tu app 'theme/'
+python manage.py tailwind install
 
-# 3. Construir los estilos de Tailwind (ahora 'rimraf' existirá)
+# 3. Construir los estilos de Tailwind
 python manage.py tailwind build
 
 # 4. Recolectar archivos estáticos
